4346ac12ff01824acea7232edc6362243ebaa2c6
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-12-03 10:08:33 -0800

    Finish sensor testing script

--- sensor-tester.ino
+++ sensor-tester.ino
@@ -8,8 +8,31 @@ int pirState = LOW; // default state, no motion detected
 int val = 0;        // for reading pin status
 
 void setup() {
+
   pinMode(ledPin, OUTPUT);  // declare LED as output
   pinMode(inputPin, INPUT); // declare sensor as input
 
   Serial.begin(9600);
+
+}
+
+void loop() {
+
+  val digitalRead(inputPin);    // read input vlue
+  if (val == HIGH) {            // check if input is High
+    digitalWrite(ledPin, HIGH); // turn LED on
+    if(pirstate == LOW) {
+      Serial.println("Motion detected");
+      // Change state to high in reponse to motion detected
+      pirState = HIGH;
+    }
+  } else {
+    digitalWrite(ledPin, LOW); // turn LED off
+    if (pirState == HIGH) {
+      Serial.println("Motion ended!");
+      // Change back to resting state once motion ends
+      pirState = LOW;
+    }
+  }
+
 }
