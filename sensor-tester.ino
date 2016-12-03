2aed6d2c27847672f39b009e0f78d3589e718817
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-12-03 10:01:46 -0800

    Add initial setup method

--- sensor-tester.ino
+++ sensor-tester.ino
@@ -2,7 +2,14 @@
 Source code for testing the PIR sensor
 */
 
-int ledPin = 13;
-int inputPin = 2;
-int pirState = LOW;
-int val = 0;
+int ledPin = 13;    // pin for LED
+int inputPin = 2;   // input pin (for PIR sensor)
+int pirState = LOW; // default state, no motion detected
+int val = 0;        // for reading pin status
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  // declare LED as output
+  pinMode(inputPin, INPUT); // declare sensor as input
+
+  Serial.begin(9600);
+}
