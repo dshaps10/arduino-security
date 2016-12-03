cd7725b4d30b3dea8c002c23b2aeb6173a8ee1df
Dan Shapiro <dan.shapiro1210@gmail.com>
2016-12-03 10:21:12 -0800

    Add main file for running code

--- sensor-tester/sensor-tester.ino
+++ sensor-tester/sensor-tester.ino
@@ -1,38 +1,36 @@
 /*
-Source code for testing the PIR sensor
-*/
-
-int ledPin = 13;    // pin for LED
-int inputPin = 2;   // input pin (for PIR sensor)
-int pirState = LOW; // default state, no motion detected
-int val = 0;        // for reading pin status
-
+ * PIR sensor tester
+ */
+ 
+int ledPin = 13;                // choose the pin for the LED
+int inputPin = 2;               // choose the input pin (for PIR sensor)
+int pirState = LOW;             // we start, assuming no motion detected
+int val = 0;                    // variable for reading the pin status
+ 
 void setup() {
-
-  pinMode(ledPin, OUTPUT);  // declare LED as output
-  pinMode(inputPin, INPUT); // declare sensor as input
-
+  pinMode(ledPin, OUTPUT);      // declare LED as output
+  pinMode(inputPin, INPUT);     // declare sensor as input
+ 
   Serial.begin(9600);
-
 }
-
-void loop() {
-
-  val digitalRead(inputPin);    // read input vlue
-  if (val == HIGH) {            // check if input is High
-    digitalWrite(ledPin, HIGH); // turn LED on
-    if(pirstate == LOW) {
-      Serial.println("Motion detected");
-      // Change state to high in reponse to motion detected
+ 
+void loop(){
+  val = digitalRead(inputPin);  // read input value
+  if (val == HIGH) {            // check if the input is HIGH
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+    if (pirState == LOW) {
+      // we have just turned on
+      Serial.println("Motion detected!");
+      // We only want to print on the output change, not state
       pirState = HIGH;
     }
   } else {
-    digitalWrite(ledPin, LOW); // turn LED off
-    if (pirState == HIGH) {
+    digitalWrite(ledPin, LOW); // turn LED OFF
+    if (pirState == HIGH){
+      // we have just turned of
       Serial.println("Motion ended!");
-      // Change back to resting state once motion ends
+      // We only want to print on the output change, not state
       pirState = LOW;
     }
   }
-
 }
